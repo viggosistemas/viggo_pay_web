4870f22d173d138b7566a010ce1866fe1319a42a
arthurmdros <arthurmedeiros2005@gmail.com>
2024-02-07 09:23:12 -0300

    Dashboard page update

--- lib/dashboard/ui/dashboard_page.dart
+++ lib/dashboard/ui/dashboard_page.dart
@@ -1,44 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:viggo_pay_admin/widgets/app_bar.dart';
-import 'package:viggo_pay_admin/widgets/main_drawer.dart';
+import 'package:viggo_pay_admin/app_builder/ui/app_builder.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
   @override
-  Widget build(context) {
-    // showInfoMessage(
-    //   context,
-    //   20000,
-    //   Colors.red,
-    //   'Seja bem-vindo, usuário!',
-    //   'X',
-    //   () {},
-    //   Colors.white,
-    // );
-    return Scaffold(
-      appBar: const AppBarPrivate(),
-      drawer: MainDrawer(
-        onSelectScreen: (String identifier) {
-          Navigator.pop(context);
-          if (identifier == 'filters') {
-            // Navigator.of(context).pushReplacement(
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (ctx) => Text('aqui'),
-              ),
-            );
-          }
-        },
-      ),
-      body: Container(
-        child: const Text(
-          'Dashboard',
-          style: TextStyle(
-            color: Colors.black,
+  Widget build(BuildContext context) {
+    // final viewModel = Provider.of<AppBuilderViewModel>(context);
+
+    // return StreamBuilder<Object>(
+    //     stream: viewModel.isLogged,
+    //     builder: (context, snapshot) {
+    //       if (snapshot.data != null && snapshot.data == true) {
+    //         showInfoMessage(
+    //           context,
+    //           2,
+    //           Colors.green,
+    //           'Seja bem-vindo, usuário!',
+    //           'X',
+    //           () {},
+    //           Colors.white,
+    //         );
+    //       }
+
+    return AppBuilder(
+      child: Expanded(
+        child: Container(
+          child: const Text(
+            'Dashboard',
+            style: TextStyle(
+              color: Colors.black,
+            ),
           ),
         ),
       ),
     );
+    // }
+    // );
   }
 }
