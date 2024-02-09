c0e6047f2b3a3bcb813fba3ad59da00185344bc3
arthurmdros <arthurmedeiros2005@gmail.com>
2024-02-09 13:23:50 -0300

    Criação do pacote de transações entre contas

---
+++ lib/transacao_contas/ui/historico_transacoes_view_model.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:viggo_pay_core_frontend/domain/domain/usecases/search_domain_by_name_use_case.dart';
+import 'package:viggo_pay_core_frontend/domain/domain/usecases/set_domain_use_case.dart';
+import 'package:viggo_pay_core_frontend/network/network_exceptions.dart';
+
+class TransacaoContaViewModel extends ChangeNotifier {
+  final SearchDomainByNameUseCase searchDomainByName;
+  final SetDomainUseCase setDomain;
+  bool isLoading = false;
+
+  TransacaoContaViewModel({
+    required this.searchDomainByName,
+    required this.setDomain,
+  });
+
+  void _notifyLoading() {
+    isLoading = !isLoading;
+    notifyListeners();
+  }
+
+  String? _handleException(NetworkException exception) {
+    String? error;
+    if (exception.runtimeType == NotFound) {
+      error = 'Domínio não encontrado';
+    } else {
+      error = exception.message;
+    }
+
+    return error;
+  }
+
+  void onSearch(String searchTerm, Function onError, Function onSucess) async {
+    _notifyLoading();
+
+    var result = await searchDomainByName.invoke(name: searchTerm);
+    if (result.isRight) {
+      setDomain.invoke(result.right);
+      _notifyLoading();
+      onSucess();
+    } else {
+      var error = _handleException(result.left);
+      _notifyLoading();
+      onError(error);
+    }
+  }
+}
