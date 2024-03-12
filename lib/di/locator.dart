e0882e976bff992c0ef5ecb63e7c3b50504531a3
arthurmdros <arthurmedeiros2005@gmail.com>
2024-03-12 15:10:22 -0300

    Adição de novos locators para pacotes do sysadmin e do admin

--- lib/di/locator.dart
+++ lib/di/locator.dart
@@ -2,13 +2,22 @@ import 'package:get_it/get_it.dart';
 import 'package:viggo_pay_admin/app_builder/locator.dart';
 import 'package:viggo_pay_admin/app_builder/ui/app_components/header-search/locator.dart';
 import 'package:viggo_pay_admin/app_builder/ui/app_components/pop_menu/locator.dart';
+import 'package:viggo_pay_admin/dashboard/locator.dart';
+import 'package:viggo_pay_admin/domain/locator.dart';
 import 'package:viggo_pay_admin/domain_account/locator.dart';
+import 'package:viggo_pay_admin/extrato/locator.dart';
 import 'package:viggo_pay_admin/forget_password/locator.dart';
-import 'package:viggo_pay_admin/historico/locator.dart';
+import 'package:viggo_pay_admin/funcionario/locator.dart';
 import 'package:viggo_pay_admin/lazy_loading/locator.dart';
 import 'package:viggo_pay_admin/login/locator.dart';
+import 'package:viggo_pay_admin/matriz/locator.dart';
+import 'package:viggo_pay_admin/parceiro/locator.dart';
+import 'package:viggo_pay_admin/pay_facs/locator.dart';
 import 'package:viggo_pay_admin/pix_to_send/locator.dart';
+import 'package:viggo_pay_admin/role/locator.dart';
+import 'package:viggo_pay_admin/route/locator.dart';
 import 'package:viggo_pay_admin/sync/locator.dart';
+import 'package:viggo_pay_admin/user/locator.dart';
 import 'package:viggo_pay_core_frontend/di/locator.dart';
 
 var locator = GetIt.instance;
@@ -21,11 +30,22 @@ Future<void> setupLocator() async {
   LoginLocator().setup();
   ForgetPasswordLocator().setup();
   SyncLocator().setup();
+  //sysadmin
+  DomainLocator().setup();
+  UsersLocator().setup();
+  RoleLocator().setup();
+  RouteLocator().setup();
 
+  //admin
+  MatrizAccountLocator().setup();
   DomainAccountLocator().setup();
   PixToSendLocator().setup();
-  HistoricoTransacaoLocator().setup();
-  
+  ExtratoLocator().setup();
+  PayFacsLocator().setup();
+  FuncionarioLocator().setup();
+  ParceiroLocator().setup();
+  DashboardLocator().setup();
+
   //appBuilder
   AppBuilderLocator().setup();
   PopMenuLocator().setup();
