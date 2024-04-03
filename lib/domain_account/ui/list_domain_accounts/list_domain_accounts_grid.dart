8501dc7f37622a55c7969fed4de0fc61e01330bb
arthurmdros <arthurmedeiros2005@gmail.com>
2024-04-03 15:12:06 -0300

    Aplicando dinãmica de nova classe de container com constraints de dimensão - Domain_account menu

--- lib/domain_account/ui/list_domain_accounts/list_domain_accounts_grid.dart
+++ lib/domain_account/ui/list_domain_accounts/list_domain_accounts_grid.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:viggo_core_frontend/util/list_options.dart';
-import 'package:viggo_pay_admin/app_builder/ui/app_components/data_table_paginated.dart';
 import 'package:viggo_pay_admin/app_builder/ui/app_components/header-search/ui/header_search_main.dart';
+import 'package:viggo_pay_admin/app_builder/ui/app_components/list-view-data/card/list_view_mode_card.dart';
+import 'package:viggo_pay_admin/app_builder/ui/app_components/list-view-data/table/data_table_paginated.dart';
 import 'package:viggo_pay_admin/components/hover_button.dart';
 import 'package:viggo_pay_admin/components/progress_loading.dart';
 import 'package:viggo_pay_admin/di/locator.dart';
@@ -20,8 +23,7 @@ class ListDomainAccountsGrid extends StatefulWidget {
 }
 
 class _ListDomainAccountsGridState extends State<ListDomainAccountsGrid> {
-  ListDomainAccountViewModel viewModel =
-      locator.get<ListDomainAccountViewModel>();
+  ListDomainAccountViewModel viewModel = locator.get<ListDomainAccountViewModel>();
 
   static const domainAccountsValidActions = [
     {
@@ -146,18 +148,15 @@ class _ListDomainAccountsGridState extends State<ListDomainAccountsGrid> {
         if (element['type'] == 'text') {
           fieldValue = '%${element['value']}%';
         } else if (element['type'] == 'cnpj') {
-          fieldValue = element['value']
-              .replaceAll('.', '')
-              .replaceAll('-', '')
-              .replaceAll('/', '');
+          fieldValue = element['value'].replaceAll('.', '').replaceAll('-', '').replaceAll('/', '');
         } else {
           fieldValue = element['value'];
         }
-        if(element['type'] == 'bool') {
+        if (element['type'] == 'bool') {
           filters.addEntries(
             <String, String>{element['search_field']: fieldValue == 'Sim' ? 'true' : 'false'}.entries,
           );
-        }else{
+        } else {
           filters.addEntries(
             <String, String>{element['search_field']: fieldValue}.entries,
           );
@@ -197,161 +196,206 @@ class _ListDomainAccountsGridState extends State<ListDomainAccountsGrid> {
         }
       },
     );
+
+    onConfigAccount(entity) async {
+      var config = await viewModel.getConfigInfo(entity['id']);
+      if (config == null) {
+        var result = await ConfigDomainAccounts(context: context).configDialog(viewModel.getEmptyConfigInfo(entity['id']));
+        if (result != null && result == true) {
+          viewModel.clearSelectionConfig();
+          viewModel.clearSelectedItems.invoke();
+          onReload();
+        }
+      } else {
+        var result = await ConfigDomainAccounts(context: context).configDialog(config);
+        if (result != null && result == true) {
+          viewModel.clearSelectionConfig();
+          viewModel.clearSelectedItems.invoke();
+          onReload();
+        }
+      }
+    }
+
     return StreamBuilder<List<DomainAccountApiDto>>(
       stream: viewModel.domainAccounts,
       builder: (context, snapshot) {
         if (snapshot.data == null) {
           onReload();
           return ProgressLoading(
-            color: Theme.of(context).brightness == Brightness.dark
-                ? Theme.of(context).colorScheme.secondary
-                : Theme.of(context).colorScheme.primary,
+            color: Theme.of(context).brightness == Brightness.dark ? Theme.of(context).colorScheme.secondary : Theme.of(context).colorScheme.primary,
           );
         } else {
-          List<DomainAccountApiDto> items =
-              (snapshot.data as List<DomainAccountApiDto>);
-          return SizedBox(
-            height: double.maxFinite,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  HeaderSearchMain(
-                    onSearch: onSearch,
-                    onReload: onReload,
-                    searchFields: searchFields,
-                    itemsSelect: itemSelect
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: DataTablePaginated(
-                      viewModel: viewModel,
-                      streamList: viewModel.domainAccounts,
-                      initialFilters: filters,
-                      columnsDef: const [
-                        DataColumn(
-                            label: Center(
-                                child: Text(
-                          'Matera',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ))),
-                        DataColumn(
-                            label: Center(
-                                child: Text(
-                          'Cliente',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ))),
-                        DataColumn(
-                            label: Center(
-                                child: Text(
-                          'CNPJ',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ))),
-                        DataColumn(
-                            label: Center(
-                                child: Text(
-                          'Possui chave PIX',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ))),
-                        DataColumn(
-                            label: Center(
-                                child: Text(
-                          'Possui taxa cadastrada',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ))),
-                        DataColumn(
-                            label: Center(
-                                child: Text(
-                          'Status',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ))),
-                      ],
-                      fieldsData: domainAccountRowsValues,
-                      validActionsList: domainAccountsValidActions,
-                      dialogs: dialogs,
-                      items: items.map((e) {
-                        return e.toJson();
-                      }).toList(),
-                      actions: [
-                        StreamBuilder<DomainAccountConfigApiDto?>(
-                            stream: viewModel.configDomainAccount,
-                            builder: (context, snapshot) {
-                              var selecteds = viewModel.selectedItemsList
-                                  .where((e) => e.selected == true)
-                                  .toList();
-
-                              if (selecteds.isEmpty || selecteds.length > 1) {
-                                viewModel.clearSelectionConfig();
-                              }
-
-                              if (snapshot.data == null &&
-                                  selecteds.length == 1) {
-                                viewModel.getConfigInfo(selecteds[0].id);
-                              }
-
-                              return OnHoverButton(
-                                child: IconButton.outlined(
-                                  onPressed: () async {
-                                    if (selecteds.length == 1) {
-                                      if (snapshot.data == null) {
-                                        var result = await ConfigDomainAccounts(
-                                                context: context)
-                                            .configDialog(
-                                                viewModel.getEmptyConfigInfo(
-                                                    selecteds[0].id));
-                                        if (result != null && result == true) {
-                                          viewModel.clearSelectionConfig();
-                                          viewModel.clearSelectedItems.invoke();
-                                        }
-                                      } else {
-                                        var result = await ConfigDomainAccounts(
-                                                context: context)
-                                            .configDialog(snapshot.data!);
-                                        if (result != null && result == true) {
-                                          viewModel.clearSelectionConfig();
-                                          viewModel.clearSelectedItems.invoke();
-                                        }
-                                      }
-                                    }
-                                  },
-                                  tooltip: 'Configurações',
-                                  icon: const Icon(
-                                    Icons.settings,
-                                  ),
-                                ),
-                              );
-                            }),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                      ],
+          List<DomainAccountApiDto> items = (snapshot.data as List<DomainAccountApiDto>);
+          return LayoutBuilder(builder: (context, constraints) {
+            return SizedBox(
+              height: double.maxFinite,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 10,
                     ),
-                  ),
-                  // DataTableNotPaginated(
-                  //   viewModel: viewModel,
-                  //   items: items,
-                  // ),
-                ],
+                    HeaderSearchMain(onSearch: onSearch, onReload: onReload, searchFields: searchFields, itemsSelect: itemSelect),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: constraints.maxWidth <= 960
+                          ? Padding(
+                              padding: const EdgeInsets.all(50.0),
+                              child: ListViewCard(
+                                onReloadData: onReload,
+                                viewModel: viewModel,
+                                dialogs: dialogs,
+                                actions: [
+                                  PopupMenuItem<dynamic>(
+                                    value: {'action': onConfigAccount, 'type': 'EDIT'},
+                                    child: Row(
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "Configurações",
+                                          style: TextStyle(
+                                            color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black,
+                                          ),
+                                        ),
+                                        Icon(
+                                          Icons.settings,
+                                          size: 18,
+                                          color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                                fieldsData: 'client_name',
+                                fieldsSubtitleData: 'tem_taxa',
+                                validActionsList: domainAccountsValidActions,
+                                items: items.map((e) {
+                                  return e.toJson();
+                                }).toList(),
+                              ),
+                            )
+                          : DataTablePaginated(
+                              viewModel: viewModel,
+                              streamList: viewModel.domainAccounts,
+                              initialFilters: filters,
+                              columnsDef: const [
+                                DataColumn(
+                                    label: Center(
+                                        child: Text(
+                                  'Matera',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ))),
+                                DataColumn(
+                                    label: Center(
+                                        child: Text(
+                                  'Cliente',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ))),
+                                DataColumn(
+                                    label: Center(
+                                        child: Text(
+                                  'CNPJ',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ))),
+                                DataColumn(
+                                    label: Center(
+                                        child: Text(
+                                  'Possui chave PIX',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ))),
+                                DataColumn(
+                                    label: Center(
+                                        child: Text(
+                                  'Possui taxa cadastrada',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ))),
+                                DataColumn(
+                                    label: Center(
+                                        child: Text(
+                                  'Status',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ))),
+                              ],
+                              fieldsData: domainAccountRowsValues,
+                              validActionsList: domainAccountsValidActions,
+                              dialogs: dialogs,
+                              items: items.map((e) {
+                                return e.toJson();
+                              }).toList(),
+                              actions: [
+                                StreamBuilder<DomainAccountConfigApiDto?>(
+                                    stream: viewModel.configDomainAccount,
+                                    builder: (context, snapshot) {
+                                      var selecteds = viewModel.selectedItemsList.where((e) => e.selected == true).toList();
+
+                                      if (selecteds.isEmpty || selecteds.length > 1) {
+                                        viewModel.clearSelectionConfig();
+                                      }
+
+                                      if (snapshot.data == null && selecteds.length == 1) {
+                                        viewModel.getConfigInfo(selecteds[0].id);
+                                      }
+
+                                      return OnHoverButton(
+                                        child: IconButton.outlined(
+                                          onPressed: () async {
+                                            if (selecteds.length == 1) {
+                                              if (snapshot.data == null) {
+                                                var result = await ConfigDomainAccounts(context: context)
+                                                    .configDialog(viewModel.getEmptyConfigInfo(selecteds[0].id));
+                                                if (result != null && result == true) {
+                                                  viewModel.clearSelectionConfig();
+                                                  viewModel.clearSelectedItems.invoke();
+                                                  onReload();
+                                                }
+                                              } else {
+                                                var result = await ConfigDomainAccounts(context: context).configDialog(snapshot.data!);
+                                                if (result != null && result == true) {
+                                                  viewModel.clearSelectionConfig();
+                                                  viewModel.clearSelectedItems.invoke();
+                                                  onReload();
+                                                }
+                                              }
+                                            }
+                                          },
+                                          tooltip: 'Configurações',
+                                          icon: const Icon(
+                                            Icons.settings,
+                                          ),
+                                        ),
+                                      );
+                                    }),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                              ],
+                            ),
+                    ),
+                    // DataTableNotPaginated(
+                    //   viewModel: viewModel,
+                    //   items: items,
+                    // ),
+                  ],
+                ),
               ),
-            ),
-          );
+            );
+          });
         }
       },
     );
