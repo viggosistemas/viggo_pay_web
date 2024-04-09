501e8817886e0e35dfc7fd911834b609b24306d3
arthurmdros <arthurmedeiros2005@gmail.com>
2024-04-09 11:33:14 -0300

    Refatorando apis do projeto para seguirem o padrão do BASE API

--- lib/funcionario/data/remote/funcionario_api.dart
+++ lib/funcionario/data/remote/funcionario_api.dart
@@ -2,10 +2,9 @@
 
 import 'dart:convert';
 
-import 'package:http/http.dart' as http;
-import 'package:viggo_pay_admin/funcionario/data/models/response.dart';
 import 'package:viggo_core_frontend/base/base_api.dart';
 import 'package:viggo_core_frontend/network/network_exceptions.dart';
+import 'package:viggo_pay_admin/funcionario/data/models/response.dart';
 
 class FuncionarioApi extends BaseApi {
   FuncionarioApi({required super.settings});
@@ -15,14 +14,9 @@ class FuncionarioApi extends BaseApi {
   Future<FuncionariosResponse> getEntitiesByParams(
     Map<String, dynamic> params,
   ) async {
-    String url = '$baseUrl$ENDPOINT';
-    Map<String, String> headers = getHeaders();
+    String url = handleFilters(ENDPOINT, params);
 
-    // Monta os filtros da entidade baseado nos parâmetros solicitados
-    url = handleFilters(url, params);
-
-    // Envia requisição e trata retorno
-    var response = await http.get(Uri.parse(url), headers: headers);
+    var response = await get(url);
     switch (response.statusCode) {
       case 200:
         Map<String, dynamic> json = jsonDecode(response.body);
@@ -41,11 +35,11 @@ class FuncionarioApi extends BaseApi {
   ) async {
     String id = params['id'];
     String? include = params['include'];
-    Map<String, String> headers = getHeaders();
-
     String url = '$baseUrl$ENDPOINT/$id';
+
     if (include != null) url = '$url?include=$include';
-    var response = await http.get(Uri.parse(url), headers: headers);
+
+    var response = await get(url);
     switch (response.statusCode) {
       case 200:
         Map<String, dynamic> json = jsonDecode(response.body);
@@ -62,16 +56,10 @@ class FuncionarioApi extends BaseApi {
   Future<FuncionarioResponse> updateEntity(Map<String, dynamic> params) async {
     String id = params['id'];
     Map<String, dynamic> body = params['body'];
-    Map<String, String> headers = getHeaders();
 
-    body = cleanEntity(body);
-    String url = '$baseUrl$ENDPOINT/$id';
+    String url = '$ENDPOINT/$id';
 
-    var response = await http.put(
-      Uri.parse(url),
-      headers: headers,
-      body: jsonEncode(body),
-    );
+    var response = await put(url, body: body);
     switch (response.statusCode) {
       case 200:
         Map<String, dynamic> json = jsonDecode(response.body);
@@ -87,16 +75,8 @@ class FuncionarioApi extends BaseApi {
 
   Future<FuncionarioResponse> createEntity(Map<String, dynamic> params) async {
     Map<String, dynamic> body = params['body'];
-    Map<String, String> headers = getHeaders();
 
-    body = cleanEntity(body);
-    String url = '$baseUrl$ENDPOINT';
-
-    var response = await http.post(
-      Uri.parse(url),
-      headers: headers,
-      body: jsonEncode(body),
-    );
+    var response = await post(ENDPOINT, body: body);
     switch (response.statusCode) {
       case 201:
         Map<String, dynamic> json = jsonDecode(response.body);
